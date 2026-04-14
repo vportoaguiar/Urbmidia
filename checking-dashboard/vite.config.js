12c19773cf038fda4711c6de5ea94a7c4d3c1cd4
vportoaguiar <vportoaguiar@gmail.com>
2026-04-14 15:21:04 -0300

    Update vite.config.js

--- checking-dashboard/vite.config.js
+++ checking-dashboard/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Urbmidia/checking-dashboard/',
+  base: '/Urbmidia/',
   build: {
     outDir: 'dist',
-    sourcemap: false,
   }
 })
